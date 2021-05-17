96694d44d2a9269f129a01245e43d3b4f50b932b
vats1812 <68377043+vats1812@users.noreply.github.com>
2021-05-17 13:25:05 +0530

    port edit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const app = require('express')()
 const httpServer = require("http").createServer(app);
 const io = require("socket.io", "socket.io-client")(httpServer, {
-    origin: '*',
+    cors: {
+        origin: '*',
+    }
 });
 // const server = require('http').createServer(app)
 //     // const io = require('socket.io')(server, {
